687fd945f7a18ae8268a439a37a9e79980221204
saydaliyev elmurod <saydaliyevelmurod01@gmail.com>
2024-03-26 23:01:13 +0500

    finished

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,41 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            StringBuilder text = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+            return parseToObject(text.toString());
+        } catch (IOException e) {
+            System.out.println("File not found or input error ");
+        }
+        return null;
+    }
+
+    private Profile parseToObject(String text) {
+        Profile profile = new Profile();
+        String[] arr = text.split("\n");
+        for (String line : arr) {
+            String[] a = line.split(": ");
+            if ("Name".equals(a[0])) {
+                profile.setName(a[1]);
+            } else if ("Age".equals(a[0])) {
+                profile.setAge(Integer.parseInt(a[1]));
+            } else if ("Email".equals(a[0])) {
+                profile.setEmail(a[1]);
+            } else if ("Phone".equals(a[0])) {
+                profile.setPhone(Long.parseLong(a[1]));
+            }
+        }
+        return profile;
     }
 }
